fbc40cfc6e6b23fab7e80965fe93c582817892ac
HaripriyaaSaisubramanian <sharitpiyaa17@gmail.com>
2019-05-30 08:04:33 +0530

    new one

---
+++ src/org/greens/GreensTech.java
@@ -0,0 +1,13 @@
+package org.greens;
+
+public class GreensTech {
+
+	public void Course() {
+System.out.println("Classes starts at 8 am");
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.Course();
+		
+	}
+}
